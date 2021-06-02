6a9a840951331d620db73474e774b34bb3492cd4
Mahamat Abakar <mahamat.abakar-hassan@epitech.eu>
2021-06-02 10:12:48 +0200

    final

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import {HashRouter, Route, Switch} from 'react-router-dom'
+import Album from './pages/album/Album';
+import AlbumShow from './pages/album/AlbumShow';
+import Artist from './pages/artist/Artist';
+import Atistinfo from './pages/artist/Atistinfo';
+
+
+
+function App() {
+  return (
+    <HashRouter>
+      <div className="container-fluid">
+           <Switch>
+            <Route exact path="/" component={Album}/>
+            <Route exact path="/album" component={Album}/>
+            <Route exact path="/artist" component={Artist}/>
+            <Route  path="/artist_info" component={Atistinfo}/>
+            <Route exact path="/album/:id" component={AlbumShow}/>
+           </Switch>
+      </div>
+    </HashRouter>
+  );
+}
+export default App;
---
+++ frontend/src/pages/artist/Artist.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class Artist extends Component{
+   constructor(){
+      super();
+      this.state = {
+         artists: [],
+         page: 1,
+         nb_all: 0
+      }
+      this.getArtist();
+   }
+
+   /*async getArtist(id){
+      let nameArtist = await axios.get(`http://127.0.0.1:8000/api/artists/${id}`).then(res => {
+         return res.data.name
+      })
+      
+      return nameArtist
+   }*/
+
+   getArtist(){
+      axios.get(`http://127.0.0.1:8000/api/artists?page=${this.state.page}`).then(res => {
+         return this.setState({
+           artists: res.data["hydra:member"],
+           nb_all: res.data["hydra:totalItems"]
+         });
+      });
+   }
+
+   renderArtist(){
+       
+      return this.state.artists.map((artist) => {
+         return (
+            <div key={artist} className="info_artist">
+               <Link to={`/artist_info?id=${artist.id}`}>
+                  <h3>{artist.name}</h3>
+                  <img className="image_artist" src={artist.photo} alt="photo artist" />
+               </Link>
+               <p>{artist.description}</p>
+            </div>
+         )
+      })
+   }
+
+   nextPage(){
+      if (this.state.page < Math.ceil(this.state.nb_all / 15)){
+         let nb_page = this.state.page + 1;
+         this.setState({
+            page: nb_page
+         }, () => {
+            this.getArtist();
+         })
+      }
+   }
+
+   prevPage(){
+      if (this.state.page > 1){
+         let nb_page = this.state.page - 1;
+         this.setState({
+            page: nb_page
+         }, () => {
+            this.getArtist();
+         })
+      }
+   }
+
+   render() {
+     
+      return (
+         <div className="pageArtist">
+            <h1>Liste des artiste disponibles dans la plateforme</h1> 
+            <p>Nombre d'artistes trouvés: {this.state.nb_all}</p>
+            <button class="btn1 btn btn-primary" onClick={this.prevPage.bind(this)}>Prev</button>
+            <button class="btn2 btn btn-primary" onClick={this.nextPage.bind(this)}>Next</button>
+            <div>{this.renderArtist()}</div>
+         </div>
+      )
+   }
+}
+export default Artist;
+
